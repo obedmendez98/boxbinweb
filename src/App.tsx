615be40c3384abc0ff28f509125ff76f626145f4
OBED MENDEZ MARTINEZ <mendezobed98@gmail.com>
2025-07-19 11:37:40 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { AuthProvider } from '@/context/AuthContext';
 import Routes from '@/routes';
 import './App.css';
+import { Toaster } from 'sonner';
 
 function App() {
   return (
     <AuthProvider>
       <Routes />
+      <Toaster />
     </AuthProvider>
   );
 }
